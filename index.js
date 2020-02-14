03bbe9b5224eb63f632e44be627cbbfdcb52574d
Ana Flavia Foppa <anaffoppa@gmail.com>
2020-02-14 11:59:03 +0000

    Lab completo

--- index.js
+++ index.js
@@ -11,9 +11,52 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+  .then(self => {
+    return self.connection.dropDatabase(); // APENAS PARA O  EXERCICIO - pelo unique do title
+  })
   .then(() => {
-    console.log(`Connected to the database: "${x.connections[0].name}"`);
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: 'SOBRECOXA ASSADA COM ARROZ E LENTILHA',
+      level: 'UltraPro Chef',
+      ingredients: ['3 sobrecoxas de frango com pele e osso', '1 maçãs fuji', '1 cabeça de alho'],
+      cuisine: 'Brazilian',
+      dishType: 'Dish',
+      image:
+        'https://panelinha-sitenovo.s3.sa-east-1.amazonaws.com/receita/1579808151261-sobrecoxa-assada.jpg',
+      duration: 120,
+      creator: 'Rita Lobo',
+      created: '2017-05-12'
+    });
+  })
+  .then(recipeDocument => {
+    console.log(recipeDocument.title);
+
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    return Recipe.find({}, { title: 1, _id: 0 });
+  })
+  .then(title => {
+    console.log(title);
+  })
+  .then(() => {
+    return Recipe.updateOne({ title: 'Rigatoni alla Genovese' }, { duration: 100 });
+  })
+  .then(() =>{
+    console.log('Rigatoni alla Genovese was updated');
+  })
+  .then(() => {
+    return Recipe.deleteOne({ title: 'Carrot Cake' });
+  })
+  .then(() =>{
+    console.log('Carrot Cake was deleted')
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .then(() =>{
+    console.log('Has disconnected');
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,7 +3,65 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+  title: {
+    type: String,
+    require: true,
+    unique:true
+  },
+  level:{
+    type: String,
+    enum:['Easy Peasy', 'Amateur Chef','UltraPro Chef']
+  },
+  ingredients:{
+    type: Array
+  },
+  cuisine:{
+    type: String
+  },
+  dishType:{
+    type:String,
+    enum:['Breakfast','Dish','Snack','Drink','Dessert','Other']
+  },
+  image:{
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration:{
+    type: Number,
+    min: 0
+  },
+  creator:{
+    type: String
+  },
+  created:{
+    type: Date,
+    default: Date.now
+  }
+
 });
+
+
+/*
+
+const bookSchema = new mongoose.Schema({
+  title: String, //typeof value, need to be with upperCase
+  author: {
+    type:String,
+    required: true //caso o autor não for preenchido, não será aceito no DB
+  },
+  release: Number,
+  pages: {
+    type: Number,
+    max: 500 //maximo valor de 500
+  },
+  genre: {
+    type:String,
+    enum:['romance','drama','mistery'] //apenas estes valores serão aceitos
+  }, //typeof do valor de dentro da array
+  available: Boolean
+});*/
+
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
